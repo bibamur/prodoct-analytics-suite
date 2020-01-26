e17802bd6dc343a699f77de5b8b2e34bec41ea4d
Dmitrii Murzin <bibamur@gmail.com>
2020-01-26 07:21:28 -0500

    1. Code for Funnels is added. 2. Defect in time_periods data is fixed. 3. Histogram and scatter code is changed to accomodate histogram for all events (not just for the first user period)

--- Tools_for_user_cohorts/histogram_and_scatter.py
+++ Tools_for_user_cohorts/histogram_and_scatter.py
@@ -18,28 +18,37 @@ import matplotlib.lines as mlines
 all_user_events_df = pd.read_csv('..\\data\\user_events_df.csv')
 all_user_events_df['date'] = all_user_events_df['date'].astype(str).str[:-6] # remove timestamp
 all_user_events_df['date'] = pd.to_datetime(all_user_events_df['date'])
-all_user_events_df.set_index('date', inplace = True)
+
 time_periods_df = pd.read_csv('..\\data\\time_periods_df.csv', index_col = 0)
-"""
-sort out first time period for user
-"""
-payment_events_df = pd.read_csv('..\\data\\payment_events_df.csv', index_col = 0)
+time_periods_df['start_date'] = pd.to_datetime(time_periods_df['start_date'])
+time_periods_df['end_date'] = pd.to_datetime(time_periods_df['end_date'])
+
+
+first_date_group = all_user_events_df.groupby('user_id')['date'].min()
+
+def get_first_season(time_stamp):
     
-user_ids = payment_events_df['user_id'].values
-time_period_ids = payment_events_df['time_period_id'].values
-user_time_periods_df = pd.DataFrame({'user_id': user_ids, 'time_period_id':time_period_ids})
-first_time_periods_group = user_time_periods_df.groupby('user_id')['time_period_id'].min()
-user_time_periods_df['first_time_period_id'] = user_time_periods_df['user_id'].map(first_time_periods_group).copy()
+    s = time_periods_df.loc[
+            (time_periods_df.start_date <= time_stamp)
+            & (time_periods_df.end_date > time_stamp),'time_period_id']
+    if len(s) == 1: return s.iloc[0]
+    else: return -1
+    
+first_user_season = first_date_group.apply(get_first_season)    
+
+user_time_periods_df = pd.DataFrame({'user_id':first_user_season.index, 'first_time_period_id':first_user_season.values})
 
 
+all_user_events_df.set_index('date', inplace = True)
 
 
-
-def calculate_user_event_counts(user_events_df):
+"""This function calculates count of events performed by user during his/her first time period"""
+def calculate_first_period_user_event_counts(user_events_df):
     all_user_event_counts = pd.Series()
     for time_period_id, start_date, end_date in zip(time_periods_df.time_period_id, time_periods_df.start_date, time_periods_df.end_date):
-        end_date = str((datetime.datetime.strptime(end_date, '%Y-%m-%d') - datetime.timedelta(1)).date()) # take previous day
-        time_period_events_df = user_events_df.loc[start_date:end_date]
+        #end_date = str((datetime.datetime.strptime(end_date, '%Y-%m-%d') - datetime.timedelta(1)).date()) # take previous day
+        end_date = end_date - datetime.timedelta(1)
+        time_period_events_df = user_events_df.loc[str(start_date):str(end_date)]
         users = user_time_periods_df.loc[user_time_periods_df['first_time_period_id'] == time_period_id,'user_id'].unique() # get all users with first time period equal to the time period in iteration
         #print(users)    
         event_counts = time_period_events_df.loc[time_period_events_df['user_id'].isin(users), 'user_id'].value_counts() # get number of events for each user within period in iteration
@@ -51,6 +60,9 @@ def calculate_user_event_counts(user_events_df):
         all_user_event_counts = all_user_event_counts.append(zero_event_users)
     return all_user_event_counts 
 
+
+def calculate_all_user_event_counts(user_events_df):
+    return user_events_df['user_id'].value_counts()
 
 
 
@@ -113,7 +125,8 @@ event_1 = {
         }
             
 user_events_df = all_user_events_df.loc[(all_user_events_df[list(event_1)] == pd.Series(event_1)).all(axis = 1)]
-all_user_event_counts_social = calculate_user_event_counts(user_events_df)
+all_user_event_counts_social = calculate_first_period_user_event_counts(user_events_df)
+#all_user_event_counts_social = calculate_all_user_event_counts(user_events_df)
 plot_histogram(all_user_event_counts_social, 'Number of Users who have completed certain number of Social tasks', 'Number of completed tasks','Frequency')
 
 
@@ -125,27 +138,25 @@ event_2 = {
         }
 
 user_events_df = all_user_events_df.loc[(all_user_events_df[list(event_2)] == pd.Series(event_2)).all(axis = 1)]
-all_user_event_counts_course = calculate_user_event_counts(user_events_df)
-all_user_event_counts_course[all_user_event_counts_course>70] = 70 # 9 outliers with more than 70 tasks will be marked as they have 70 tasks (to reduce x-axis) 
+all_user_event_counts_course = calculate_first_period_user_event_counts(user_events_df)
+#all_user_event_counts_course = calculate_all_user_event_counts(user_events_df)
+all_user_event_counts_course[all_user_event_counts_course>200] = 200 # outliers with more than 200 tasks will be marked as they have 200 tasks (to reduce x-axis) 
 plot_histogram(all_user_event_counts_course, 'Number of Users who have completed certain number of Course tasks', 'Number of completed tasks','Frequency')
 
 
-# Put together DataFrame with all necessary data
+# Put together DataFrame with all necessary data for bivariate analysis plot
+all_user_event_counts_social.name = event_1_key
+all_user_event_counts_course.name = event_2_key
 
 
-bivariate_analysis_dict = {
-        event_1_key : all_user_event_counts_social.sort_index(),
-        event_2_key : all_user_event_counts_course.sort_index(),
-        'Number of occurencies': np.zeros(len(all_user_event_counts_course))
-        }
+bivariate_analysis_df= pd.concat([all_user_event_counts_social, all_user_event_counts_course], axis = 1).fillna(0)
+bivariate_analysis_df.sort_index(inplace=True)
 
 
-bivariate_analysis_df = pd.DataFrame(bivariate_analysis_dict, index = all_user_event_counts_social.sort_index().index )
-bivariate_analysis_df['Number of occurencies'] = bivariate_analysis_df.groupby(list(bivariate_analysis_df.columns[:-1])).transform('count') # number of occurencies is the count of users that have the same number of events ()
+num_occurencies = bivariate_analysis_df.groupby(list(bivariate_analysis_df.columns)).size().reset_index()
+num_occurencies.columns = [event_1_key,event_2_key,'Number of occurencies']
 
-
-
-#bivariate_analysis_df = bivariate_analysis_df.loc[~ ((bivariate_analysis_df[key1]==0) & (bivariate_analysis_df[key2]==0))]
+bivariate_analysis_df = bivariate_analysis_df.merge(num_occurencies, how = 'left', on = [event_1_key,event_2_key])
 
 plot_scatter(bivariate_analysis_df, 'Social vs Course tasks', event_2_key, event_1_key)
 
@@ -153,6 +164,7 @@ plot_scatter(bivariate_analysis_df, 'Social vs Course tasks', event_2_key, event
 
     
 #all_user_event_counts.hist(bins=120)
+
 """ 
 #Calculate cohorts:
 cohort_1_not_active = all_user_event_counts_course[all_user_event_counts_course<=4]
