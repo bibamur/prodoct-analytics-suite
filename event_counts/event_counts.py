81816b3f623c17466e5c51d8951671064e854305
Dmitrii Murzin <bibamur@gmail.com>
2020-04-07 11:15:29 -0400

    For Code Review

--- event_counts/event_counts.py
+++ event_counts/event_counts.py
@@ -1,112 +1,137 @@
 # -*- coding: utf-8 -*-
-
+from typing import Dict, List
 import pandas as pd
 import matplotlib.pyplot as plt
 import json
+from dataclasses import dataclass
+import os
+
+
+@dataclass
+class EventType:
+    name: str
+    conditions: Dict
+
 
 # Load metadata
-with open('..\\data\\events_metadata.json', 'r') as f:
+data_path = '../data'
+events_metadata_filename = 'events_metadata.json'
+periods_metadata_filename = 'periods_metadata.json'
+
+with open(os.path.join(data_path, events_metadata_filename), 'r') as f:
     events_metadata = json.load(f)
 
-with open('..\\data\\periods_metadata.json', 'r') as f:
+with open(os.path.join(data_path, periods_metadata_filename), 'r') as f:
     periods_metadata = json.load(f)
-
 
 # Set string constants from events metadata
 events_data_path = events_metadata['events_data_path']
 timestamp_column = events_metadata['timestamp_column']
 user_id_column = events_metadata['user_id_column']
 
-# Set string constants from events metadata
+# Set string constants from periods metadata
 periods_data_path = periods_metadata['periods_data_path']
 start = periods_metadata['period_start']
 end = periods_metadata['period_end']
 period_id_str = periods_metadata['period_id']
 
-# Set string constants needed to parse the event_counts_config.json
-event_counts_config_path = '..\\event_counts\\event_counts_config.json'
-events_str = 'events'
-event_name_str = 'name'
-event_conditions_str = 'conditions'
+# Prepare data
+events_data = pd.read_csv(events_data_path)  # Load events data
+events_data[timestamp_column] = events_data[timestamp_column].astype(str).str[:-6]  # Remove timestamp from the date
+events_data[timestamp_column] = pd.to_datetime(events_data[timestamp_column])  # Change date type to datetime
+events_data.set_index(timestamp_column, inplace=True)  # Set date as index
+
+time_periods = pd.read_csv(periods_data_path, index_col=0)
+events_data[period_id_str] = 0
 
 # Set string constant to use in the script
 counts_str = 'counts'
+event_type_str = 'event_type'
 
 
-# Data preparation
-all_events_df = pd.read_csv(events_data_path)  # Load events data
-all_events_df[timestamp_column] = all_events_df[timestamp_column].astype(str).str[:-6]  # Remove timestamp from the date
-all_events_df[timestamp_column] = pd.to_datetime(all_events_df[timestamp_column])  # Change date type to datetime
-all_events_df.set_index(timestamp_column, inplace=True)  # Set date as index
+for index, period in time_periods.iterrows():
+    period_id = period[period_id_str]
+    events_data.loc[period[start]:period[end], period_id_str] = period_id
 
-time_periods_df = pd.read_csv(periods_data_path, index_col=0)
+# Prepare event types
+config_path = '../event_counts'
+config_filename = 'event_counts_config.json'
+with open(os.path.join(config_path, config_filename), 'r') as f:
+    config_events = json.load(f)['events']
+event_types = []
+for config_event in config_events:
+    event_types.append(EventType(config_event['name'], config_event['conditions']))
 
 
-def counts_by_period(events: [{}]) -> (pd.DataFrame, pd.DataFrame):
+# Filter data by event types
+def filter_by_event_type(data: pd.DataFrame, types: List[EventType]):
+    filter = pd.Series(False, index=data.index)
+    data[event_type_str] = 0
+    for event_type in types:
+        columns = list(event_type.conditions.keys())
+        conditions = pd.Series(event_type.conditions)
+        event_filter = (data[columns] == conditions).all(axis=1)
+        data.loc[event_filter, event_type_str] = event_type.name # add event name to DataFrame
+        filter |= event_filter
+    return data[filter]
+
+
+events_data = filter_by_event_type(events_data, event_types)
+
+
+def counts_by_period(data: pd.DataFrame, types: List[EventType]) -> (pd.DataFrame):
     """
-    Gets list of events as input. Each event is described by:
-        name - event name
-        conditions - dict of conditions to apply to all_user_events_df to get this event.
-    Returns 2 DataFrames:
-        total_counts_df - total count of events per event per period
-        unique_counts_df - count of unique users that performed particular event per period
+    Calculates total number of events by event type by period
+    Args:
+        User data
+        list of event types
+    Returns:
+         DataFrame with total event counts
     """
+    tasks_counts = data.groupby([period_id_str, event_type_str])[user_id_column].agg('count')
+    tasks_counts = tasks_counts.reset_index()
+    return tasks_counts
 
-    unique_counts_df = pd.DataFrame()
-    total_counts_df = pd.DataFrame()
-    for index, period in time_periods_df.iterrows():
-        period_id = period[period_id_str]
-        all_period_events_df = all_events_df.loc[period[start]:period[end]]
-        event_names = []
-        event_totals = []
-        event_uniques = []
-        for event in events:
-            conditions = event[event_conditions_str]
-            period_events_df = all_period_events_df.loc[
-                (all_period_events_df[list(conditions)] == pd.Series(conditions)).all(axis=1)
-            ]
-            event_names.append(event[event_name_str])
-            event_totals.append(len(period_events_df))
-            event_uniques.append(len(period_events_df[user_id_column].unique()))
 
-        total_counts_dict = {
-            period_id_str: [period_id] * len(events),
-            event_name_str: event_names,
-            counts_str: event_totals
-        }
-        total_counts_df = total_counts_df.append(pd.DataFrame(total_counts_dict))
-
-        unique_counts_dict = {
-            period_id_str: [period_id] * len(events),
-            event_name_str: event_names,
-            counts_str: event_uniques
-        }
-        unique_counts_df = unique_counts_df.append(pd.DataFrame(unique_counts_dict))
-
-    return total_counts_df, unique_counts_df
+def uniques_by_period(data: pd.DataFrame, types: List[EventType]) -> (pd.DataFrame):
+    """
+    Calculates number of unique users who performed the event by event type by period
+    Args:
+        User data
+        list of event types
+    Returns:
+         DataFrame with unique users counts
+    """
+    unique_counts = data[[period_id_str, event_type_str, user_id_column]].drop_duplicates()
+    unique_counts = unique_counts.groupby([period_id_str, event_type_str])[user_id_column].agg('count')
+    unique_counts = unique_counts.reset_index()
+    return unique_counts
 
 
 def plot_event_counts(counts_df: pd.DataFrame, ax: plt.Axes, title: str):
-    """Gets DataFrame with counts, subplot and title, plots event_counts graph on the given subplot"""
+    """
+    Plots event_counts graph on the given subplot
+    Args:
+        DataFrame with counts
+        Subplot to draw the plot on
+        Title of the plot
+    """
     event_counts_df = pd.DataFrame()
-    for event_id in [event[event_name_str] for event in events_to_plot]:
-        event_counts_df = counts_df.loc[counts_df[event_name_str] == event_id]
-        ax.plot(event_counts_df[period_id_str].values, event_counts_df[counts_str].values, marker='o', label=event_id)
+    for event_type in event_types:
+        event_counts_df = counts_df.loc[counts_df[event_type_str] == event_type.name]
+        ax.plot(event_counts_df[period_id_str].values, event_counts_df[user_id_column].values, marker='o', label=event_type.name)
 
-        for period, counts in zip(event_counts_df[period_id_str].values, event_counts_df[counts_str].values):
+        for period, counts in zip(event_counts_df[period_id_str].values, event_counts_df[user_id_column].values):
             ax.annotate(counts, (period, counts))  # annotates plot with value near each data point
 
     ax.set_xticks(event_counts_df[period_id_str].unique())
     ax.set_ylabel(title)
     ax.set_title(title)
+    ax.legend()
 
 
-with open(event_counts_config_path, 'r') as f:
-    events_to_plot = json.load(f)[events_str]
-
-
-total_df, unique_df = counts_by_period(events_to_plot)
-
+total_df = counts_by_period(events_data, event_types)
+unique_df = uniques_by_period(events_data, event_types)
 
 plt.rcParams.update({'font.size': 22})
 fig, axs = plt.subplots(2)
